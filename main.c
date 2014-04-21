19aff04d116967f51de86b51c8f44cb7337b106a
Terminus <i.can.speak.c.and.basic@gmail.com>
2014-04-21 17:59:17 +0900

    change for loop macro for OpenMP and Cilk in the future

--- main.c
+++ main.c
@@ -13,12 +13,16 @@ int main()
 	printf("line sum: %d\n", ls);
 
 #define mfor(en) \
-	for(m##en=1, used##en=used; m##en<=25; m##en++, used=used##en)
+	used##en=used; \
+	for(m##en=1; m##en<=25; m##en++){
+
+#define mnext(en) \
+	used=used##en; \
+	}
 
 #define douse(en) \
 	if(used&((uint32_t)1<<m##en)){ \
 		continue; \
-	}else{ \
 		used|=(uint32_t)1<<m##en; \
 	}
 
@@ -30,14 +34,14 @@ int main()
 	}
 
 	/* left-top to right-below */
-mfor(1){
+mfor(1)
 		douse(1);
-		mfor(7){
+		mfor(7)
 			douse(7);
-			mfor(13){
+			mfor(13)
 				douse(13);
 				tcont(m1+m7+m13<ls-25-24);
-				mfor(19){
+				mfor(19)
 					douse(19);
 					tcont(m1+m7+m13+m19<ls-25);
 					m25=ls-(m19+m13+m7+m1);
@@ -53,13 +57,13 @@ mfor(1){
 					*/
 
 					/* right-top to left-below */
-					mfor(5){
+					mfor(5)
 						tcont(!(m1<m5));
 						douse(5);
-						mfor(9){
+						mfor(9)
 							douse(9);
 							tcont(m5+m9+m13<ls-25-24);
-							mfor(17){
+							mfor(17)
 								douse(17);
 								tcont(m5+m9+m13+m17<ls-25);
 								m21=ls-(m5+m9+m13+m17);
@@ -75,11 +79,11 @@ mfor(1){
 								*/
 
 								/* the first horizontal line from the top */
-								mfor(2){
+								mfor(2)
 									douse(2);
 									tcont(m1+m2+m5<ls-25-24);
 									tcont(m2+m7+m17<ls-25-24);
-									mfor(3){
+									mfor(3)
 										douse(3);
 										tcont(m1+m2+m5+m3<ls-25);
 										m4=ls-(m1+m2+m5+m3);
@@ -95,11 +99,11 @@ mfor(1){
 										*/
 	
 										/* the second horizontal line from the top */
-										mfor(6){
+										mfor(6)
 											douse(6);
 											tcont(m6+m7+m9<ls-25-24);
 											tcont(m1+m6+m21<ls-25-24);
-											mfor(8){
+											mfor(8)
 												douse(8);
 												tcont(m6+m7+m9+m8<ls-25);
 												tcont(m3+m8+m13<ls-25-24);
@@ -116,13 +120,13 @@ mfor(1){
 												*/
 	
 												/* the fifth horizontal line from the top */
-												mfor(22){
+												mfor(22)
 													douse(22);
 													tcont(m21+m22+m25<ls-25-24);
 													m12=ls-(m2+m7+m17+m22);
 													tcont(m12<=0);
 													douse(12);
-													mfor(23){
+													mfor(23)
 														douse(23);
 														tcont(m3+m8+m13+m23<ls-25);
 														tcont(m21+m22+m25+m23<ls-25);
@@ -147,7 +151,7 @@ mfor(1){
 														*/
 	
 														/* fill m11 manually and others automatically */
-														mfor(11){
+														mfor(11)
 															douse(11);
 															tcont(m11+m12+m13+m14<ls-25);
 															tcont(m1+m6+m11+m21<ls-25);
@@ -177,20 +181,20 @@ mfor(1){
 #elif 0
 															printf("%d %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d\n", m1, m2, m3, m4, m5, m6, m7, m8, m9, m10, m11, m12, m13, m14, m15, m16, m17, m18, m19, m20, m21, m22, m23, m24, m25);
 #endif
-														}
-													}
-												}
-											}
-										}
-									}
-								}
-							}
-						}
-					}
-				}
-			}
-		}
-	}
+														mnext(11)
+													mnext(23)
+												mnext(22)
+											mnext(8)
+										mnext(6)
+									mnext(3)
+								mnext(2)
+							mnext(17)
+						mnext(9)
+					mnext(5)
+				mnext(19)
+			mnext(13)
+		mnext(7)
+	mnext(1)
 
 	printf("last count: %lld\n", count);
 	return 0;
